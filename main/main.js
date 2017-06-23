67e2a663dbc981c9a711e4a9dfe404aae46e49b0
ZLH123456 <1261407236@qq.com>
2017-06-23 14:10:01 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,3 +1,11 @@
-module.exports = function main() {
-  // Write your cade here
+module.exports = function main(string a) {
+  parseInt(a); 
+  var b=0
+  if(a>9)
+  {
+    b+a/10;
+    a=a%10;
+    
+  }
+  return b;
 };
